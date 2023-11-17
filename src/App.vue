60a71cd90f231c9841d3adc26e15331153fbfef4
Jerrylac <j78779915@gmail.com>
2023-11-17 16:09:54 +0800

    add VUE

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="header">
+    <Header/>
+  </div>
+  
+  <div class="content">
+    
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+.header{
+  width: 100vw;
+  height: 15vh;
+}
+.content{
+  width: 100vw;
+  height: 85vh;
+  background-color: #635985;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+export default{
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        RouterLink,
+        // RouterView
+    }
+}
+</script>
+
+<template>
+<div class="headerSowh">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+</div>
+</template>
+
+<style scoped lang="scss">
+.headerSowh{
+    width: 100%;
+    height: 100%;
+    background-color: #E19898;
+}
+</style>
